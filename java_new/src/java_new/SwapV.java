c36608f2ee7625a7c4eafa20e4582311350f1865
Alisha.Chawla <Alisha.Chawla@IND00139.emea.akqa.local>
2020-02-26 22:03:24 +0530

    new

---
+++ java_new/src/java_new/SwapV.java
@@ -0,0 +1,29 @@
+package java_new;
+
+import java.util.Scanner;
+
+public class SwapV {
+public void swap (int a, int b)
+	
+	{
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+	System.out.println("value of a is " +a);	
+	System.out.println("value of b is " +b);	
+	}
+	
+	public static void main(String[] args)
+	{
+		SwapV obj= new SwapV();
+		Scanner s= new Scanner (System.in);
+		System.out.println("Enter value of a=");
+		int a= s.nextInt();
+		System.out.println("Enter value of b=");
+		int b= s.nextInt();
+		obj.swap(a, b);
+		
+	}
+	
+}
